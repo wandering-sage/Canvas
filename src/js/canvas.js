111da96c115d394da72fd36db1b0dfb8a7f1ed44
wandering-sage <shivam4503@gmail.com>
2020-11-25 23:52:52 +0530

    Updated

---
+++ src/js/canvas.js
@@ -0,0 +1,46 @@
+var canvas = document.querySelector(".draw");
+var c = canvas.getContext("2d");
+var size = document.querySelector(".slider");
+var color = document.querySelector(".colorChange");
+var isDrawing = false;
+var lwidth = 10;
+var bColor = "#000";
+
+canvas.width = window.innerWidth - 195;
+canvas.height = window.innerHeight - 30;
+
+size.oninput = () => (lwidth = size.value);
+color.oninput = () => (bColor = color.value);
+
+canvas.addEventListener("mousedown", mouseDown);
+canvas.addEventListener("mouseup", mouseUp);
+canvas.addEventListener("mouseout", mouseUp);
+
+canvas.addEventListener("mousemove", brushDraw);
+
+c.lineJoin = "round";
+c.lineCap = "round";
+
+function brushDraw(e) {
+	if (!isDrawing) return;
+	c.strokeStyle = bColor;
+	c.beginPath();
+	c.moveTo(lastX, lastY);
+	c.lineTo(e.offsetX, e.offsetY);
+	c.stroke();
+	lastX = e.offsetX;
+	lastY = e.offsetY;
+	if (c.lineWidth < lwidth) c.lineWidth += lwidth / 20;
+}
+
+function mouseDown(e) {
+	if (e.button != 0) return;
+	isDrawing = true;
+	lastX = e.offsetX;
+	lastY = e.offsetY;
+}
+
+function mouseUp() {
+	isDrawing = false;
+	c.lineWidth = lwidth / 4;
+}
